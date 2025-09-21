a94476eeb00507f571962a62560358ca88280486
Baygeldi <baygeldi0718@gmail.com>
2025-09-22 00:06:36 +0800

    Real time clock added

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,6 +1,19 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, useCallback} from 'react';
 
 function DigitalClock(){
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect( () => {
+        const intervalid = setInterval( () =>{
+            setTime(new Date());
+        }, 1000);
+        
+        return() => {
+            clearInterval(intervalid);
+        }
+
+    }, []);
 
     return(
         <div className="clock-container">
